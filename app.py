f1c4f8287f6b89a19308d12903b965ff0d415e46
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-01-26 17:01:54 +0000

    feat:

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from asyncio import tasks
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-if os.path.exists('env.py'):
+if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
-app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get('SECRET_KEY')
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-@app.route('/')
-@app.route('/get_tasks')
+
+@app.route("/")
+@app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template('tasks.html', tasks=tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP", "0.0.0.0"),
+    app.run(host=os.environ.get("IP", "127.0.0.1"),
             port=int(os.environ.get("PORT", 5000)),
             debug=True)
