da70702359e533e07a286d1742cd8a6c37585e93
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-01-26 19:53:30 +0000

    Docs: Add materialize and static files

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def get_tasks():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP", "127.0.0.1"),
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
             port=int(os.environ.get("PORT", 5000)),
             debug=True)
