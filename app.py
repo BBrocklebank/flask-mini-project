1c2809975b7a63e90fba6288ea4ba7e072e3b0ed
Benjamin Brocklebank <82475611+BBrocklebank@users.noreply.github.com>
2022-01-28 19:26:03 +0000

    Altered server IP, added drop down function to tasks.html

--- app.py
+++ app.py
@@ -104,6 +104,6 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP", "0.0.0.0"),
+    app.run(host=os.environ.get("IP", "127.0.0.1"),
             port=int(os.environ.get("PORT", 5000)),
             debug=True)
